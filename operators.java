ee40c7a4a2235e1d5ae1729e905b34426aa65444
doubleyoushinamide <olusholasalau@gmail.com>
2024-12-04 19:04:31 +0100

    also on day 3 for operators

---
+++ operators.java
@@ -0,0 +1,34 @@
+public class MyClass {
+    public static void main(String arg[]) {
+        int a = 5;
+        int b = a++; //post increament
+        System.out.println(".....Post Increment Stage.....");
+        System.out.println(a);
+        System.out.println(b);
+        
+        //pre increament
+        System.out.println(".....Pre Increment Stage.....");
+        b = +a; //note how a +a is a+1 while ++a is a+2
+        System.out.println(a);
+        System.out.println(b);
+        
+        System.out.println(".....Pre Decrement Stage.....");
+        b = --a; //note how a +a is a+1 while ++a is a+2
+        System.out.println(a);
+        System.out.println(b);
+        
+        System.out.println(".....Post Decrement Stage.....");
+        b = a--; //note how a +a is a+1 while ++a is a+2
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+        /**
+        * while a++, a becomes 6 while storing initial
+        * values on b.
+        * whereas, ++a is a pre-increment that increments and
+        * stores up the incremented value in b
+        **/
+        //print out
+    }
+}
