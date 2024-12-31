173333e34045e61642acb352c8d992c018d47bdf
doubleyoushinamide <olusholasalau@gmail.com>
2024-12-31 20:26:41 +0100

    Created a linked list from scratch that can addFirst, addLast and so on

---
+++ Linked List/src/LinkedList.java
@@ -0,0 +1,22 @@
+public class LinkedList {
+    private Node first;
+    private Node last;
+
+    private static class Node {
+        private int value;
+        private Node next;
+
+        public Node(){
+            var node = new Node();
+            node.value = value;
+
+        }
+    }
+
+    //addFirst
+    //addLast
+    //deleteFirst
+    //deleteLast
+    //contains
+    //indexOf
+}
