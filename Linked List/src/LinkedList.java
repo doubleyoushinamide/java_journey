a619fcb6e26e94809ef710e36145e15096eb0c1e
doubleyoushinamide <olusholasalau@gmail.com>
2024-12-31 21:27:34 +0100

    Created a linked list from scratch that can addFirst, addLast and so on

--- Linked List/src/LinkedList.java
+++ Linked List/src/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class LinkedList {
     private Node first;
     private Node last;
@@ -6,17 +8,62 @@ public class LinkedList {
         private int value;
         private Node next;
 
-        public Node(){
-            var node = new Node();
-            node.value = value;
+        public Node(int value){
+            this.value = value;
 
         }
     }
 
-    //addFirst
     //addLast
+    public void addLast(int item){
+        var node = new Node(item);
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+    //addFirst
+    public void addFirst(int item){
+        var node = new Node(item);
+        if (isEmpty())
+            first = last = node;
+        first.next = node;
+        first = node;
+    }
     //deleteFirst
+    public void deleteFirst(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+        if (first == last){
+            first = last = null;
+            return;
+        }
+        //[10 -> 20 -> 30]
+        var second = first.next;
+        first.next = null;
+        first = second;
+    }
     //deleteLast
+    public void deleteLast(){
+        //[10 -> 20 -> 30]
+        var previous = previous(last);
+        last = previous;
+        last.next = null;
+
+    }
+    private Node previous(Node node){
+        var current = first;
+        while (current != null){
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
     //contains
     //indexOf
+    private boolean isEmpty(){
+        return first == null;
+    }
 }
--- Linked List/src/Main.java
+++ Linked List/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        list.addFirst(200);
+
+    }
 }
