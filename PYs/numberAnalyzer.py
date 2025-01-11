26026fb5607c72413556a432cb42f7610978613d
doubleyoushinamide <olusholasalau@gmail.com>
2025-01-12 00:49:26 +0100

    Finished programming the numberAnalyzer game

---
+++ PYs/numberAnalyzer.py
@@ -0,0 +1,66 @@
+'''
+Welcome to the Number Analyzer and Manager!
+
+Enter a list of integers separated by spaces: 10 20 15 30 25 5
+
+Choose an option:
+1. Display all numbers
+2. Find and display the largest number
+3. Find and display the smallest number
+4. Calculate and display the sum of all numbers
+5. Display all even numbers
+6. Display all odd numbers
+7. Exit
+'''
+print("Welcome to the Number Analyzer and Manager!")
+numbers = input("Enter a list of integers separated by spaces: ")
+number_arrays = [int(x) for x in numbers.split()]
+
+while True:
+    print("Choose an option:")
+    print("1. Display all numbers")
+    print("2. Find and display the largest number")
+    print("3. Find and display the smallest number")
+    print("4. Calculate and display the sum of all numbers")
+    print("5. Display all even numbers")
+    print("6. Display all odd numbers")
+    print("7. Exit")
+    response = int(input("Please enter: "))
+    
+    if response == 1:
+        print(number_arrays)
+        continue
+    elif response == 2:
+        number_arrays.sort()
+        print(f"Largest number is: {number_arrays[-1]}")
+        continue
+    elif response == 3:
+        number_arrays.sort()
+        print(f"Smallest Number: {number_arrays[0]}")
+        continue
+    elif response == 4:
+        sum_of_numbers = 0
+        for i in number_arrays:
+            sum_of_numbers += i
+        print(f"Sum of numbers: {sum_of_numbers}")
+        continue
+    elif response == 5:
+        even_numbers = []
+        for i in number_arrays:
+            if i%2 == 0:
+                even_numbers.append(i)
+        print(f"Even numbers are: {even_numbers}")
+        continue
+    elif response == 6:
+        odd_numbers = []
+        for i in number_arrays:
+            if i%2 != 0:
+                odd_numbers.append(i)
+        print(f"Odd numbers are: {odd_numbers}")
+        continue
+    elif response == 7:
+        print("Thank you for playing the Number Analyzer game!")
+        break
+    else:
+        print("Enter a valid input!")
+        continue
