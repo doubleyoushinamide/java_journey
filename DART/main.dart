f37ea8dac92a8c52b05a461e8671127a8d193b4c
doubleyoushinamide <olusholasalau@gmail.com>
2025-01-11 12:23:51 +0100

    first impllementation in dart

--- DART/main.dart
+++ DART/main.dart
@@ -1,3 +1,15 @@
-void main(){
-    print('Hello Dart!');
+import 'dart:io';
+
+void main() {
+  print('Hello Dart!');
+
+  print('What is your name?');
+  String? name = stdin.readLineSync();
+
+  if (name != null) {
+    int number = int.parse(name);
+    print('Hello $number');
+  } else {
+    print('Empty name input');
+  }
 }
